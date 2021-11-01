7ceee31fe5955ca85ff2a9a6129a68746159faa8
antlerhead <antlerhead@hotmail.co.uk>
2021-11-01 16:30:58 +0000

    creating spec file for bookmark class

--- app.rb
+++ app.rb
@@ -2,13 +2,30 @@ require 'sinatra/base'
 require 'sinatra/reloader'
 
 class BookmarkManager < Sinatra::Base
-    configure :development do
-      register Sinatra::Reloader
-    end
-  
-    get '/' do
-      'Bookmark Manager'
-    end
-  
-    run! if app_file == $0
+  configure :development do
+    register Sinatra::Reloader
   end
+
+  get '/' do
+    'Bookmark Manager'
+  end
+
+#   get '/bookmark' do
+#     erb :bookmark
+
+get '/bookmarks' do
+    @bookmarks = Bookmarks.all
+    erb :'bookmarks'
+  end
+
+#   get '/bookmarks' do
+#     bookmarks = [
+#               "http://www.adobe.com",
+#               "http://www.google.com"
+#              ]
+  
+#     bookmarks.join
+#     erb :bookmarks
+#   end
+  run! if app_file == $0
+end
--- spec/features/viewing_bookmarks_spec.rb
+++ spec/features/viewing_bookmarks_spec.rb
@@ -1,7 +1,13 @@
 feature 'views bookmarks' do
-    scenario 'user views list of bookmarks' do
-        visit '/'
-        expect(page).to have_content "Bookmark Manager"
-    end
+  scenario 'user views list of bookmarks' do
+    visit '/'
+    expect(page).to have_content 'Bookmark Manager'  
+  end
+
+  scenario 'user views list of bookmarks' do
+    visit '/bookmarks'
+    expect(page).to have_content "http://www.adobe.com"
+    expect(page).to have_content "http://www.google.com"
+  end
 end
 
